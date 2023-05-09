e3dd291518d6f0dbacc46b39b433bc27dc8c879a
sameerpasha123 <131762422+sameerpasha123@users.noreply.github.com>
2023-05-09 16:02:32 +0530

    Create sameer.java

---
+++ sameer.java
@@ -0,0 +1,11 @@
+public class sameer{
+  public static void main(string[]args){
+    int a=50;
+    int b=54;
+    int ans;
+    
+    void add(){
+    ans=a+b;
+    System.out.println("the sum is "+ans);}
+  }
+}
